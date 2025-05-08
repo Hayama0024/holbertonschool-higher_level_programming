48cf5f9b7219afdd97dfe29079831b49a7b362b4
Hayama0024 <hayamareizo11@gmail.com>
2025-05-08 23:03:16 +0000

    task

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 def fizzbuzz():
     for i in range(1, 101, 1):
         if i % 15 == 0:
-            print("FizzBazz ", end="")
+            print("FizzBuzz ", end="")
         elif i % 3 == 0:
             print("Fizz ", end="")
         elif i % 5 == 0:
-            print("Bazz ", end="")
+            print("Buzz ", end="")
         else:
             print("{} ".format(i), end="")
