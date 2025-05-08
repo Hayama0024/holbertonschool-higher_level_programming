f74afdc23211446c12f76004ee057d5857a848a6
Hayama0024 <hayamareizo11@gmail.com>
2025-05-08 22:58:34 +0000

    task

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+def fizzbuzz():
+    for i in range(1, 101, 1):
+        if i % 15 == 0:
+            print("FIzzBazz ", end="")
+        elif i % 3 == 0:
+            print("Fizz ", end="")
+        elif i % 5 == 0:
+            print("Bazz ", end="")
+        else:
+            print("{} ".format(i), end="")
