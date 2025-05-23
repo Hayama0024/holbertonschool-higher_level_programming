eb5e663e09304cd3e8032b222eff6f624dcd5cdf
Hayama0024 <hayamareizo11@gmail.com>
2025-05-23 13:22:43 +0000

    task

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,13 +1,21 @@
+#!/usr/bin/python3
+"""Defines a Square class"""
+
+
 class Square:
+    """Represents a square"""
+
     def __init__(self, size=0):
-        self.size = size
+        self.size = size  # setterを通すことでバリデーションが動く
 
     @property
     def size(self):
+        """Retrieve size"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Set size with validation"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -15,4 +23,5 @@ class Square:
         self.__size = value
 
     def area(self):
-        return self.__size ** 2  # 面積 = size × size
+        """Return area of the square"""
+        return self.__size ** 2
