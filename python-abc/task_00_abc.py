5a3cb0a928e055fe5d4c6ea9708609b4a9455ff9
Hayama0024 <hayamareizo11@gmail.com>
2025-05-27 21:32:43 +0000

    task 00

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """Abstract base class for animals"""
+
+    @abstractmethod
+    def sound(self):
+        """Each animal must implement this method"""
+        pass
+
+
+class Dog(Animal):
+    def sound(self):
+        return "Bark"
+
+
+class Cat(Animal):
+    def sound(self):
+        return "Meow"
