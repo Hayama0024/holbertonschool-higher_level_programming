4130f1954636c1656efaf86d48827348b228d81f
Hayama0024 <hayamareizo11@gmail.com>
2025-05-29 14:27:04 +0000

    fix

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -28,7 +28,7 @@ class Circle(Shape):
         self.__radius = abs(radius)
 
     def area(self):
-        return pi * self.__radius ** 2
+        return pi * self.__radius * self.__radius
 
     def perimeter(self):
         return 2 * pi * self.__radius
@@ -39,14 +39,14 @@ class Rectangle(Shape):
     A class to define a rectangle.
     """
     def __init__(self, width, height):
-        self.__width = abs(width)
-        self.__height = abs(height)
+        self.__width = width
+        self.__height = height
 
     def area(self):
         return self.__width * self.__height
 
     def perimeter(self):
-        return 2 * (self.__width + self.__height)
+        return 2 * self.__width + 2 * self.__height
 
 
 def shape_info(obj):
