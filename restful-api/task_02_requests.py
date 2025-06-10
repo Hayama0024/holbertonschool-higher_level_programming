58487494b9b862e9ca7cbfb54af06f4d9551fe83
Hayama0024 <hayamareizo11@gmail.com>
2025-06-10 15:54:12 +0000

    task

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Module to fetch and display or save posts from JSONPlaceholder
+Module containing functions for fetching
+and printing or saving posts from JSONPlaceholder.
 """
 
 import requests
@@ -8,6 +9,7 @@ import csv
 
 
 def fetch_and_print_posts():
+    """Fetch posts and print their titles."""
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
     print("Status Code:", response.status_code)
 
@@ -18,10 +20,19 @@ def fetch_and_print_posts():
 
 
 def fetch_and_save_posts():
+    """Fetch posts and save them to a CSV file."""
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
     if response.status_code == 200:
         data = response.json()
-        with open(
-            "posts.csv", "w", newline='', encoding="utf-8"
-        ) as csvfile:
-            writer = c
+        with open("posts.csv", "w", newline='', encoding="utf-8") as csvfile:
+            writer = csv.DictWriter(
+                csvfile,
+                fieldnames=["id", "title", "body"]
+            )
+            writer.writeheader()
+            for post in data:
+                writer.writerow({
+                    "id": post["id"],
+                    "title": post["title"],
+                    "body": post["body"]
+                })
