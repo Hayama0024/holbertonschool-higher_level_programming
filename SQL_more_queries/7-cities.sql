bd0d796e99eaa894186468cd560495588a77e06a
Hayama0024 <hayamareizo11@gmail.com>
2025-06-27 13:59:47 +0000

    fix

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,11 +1,10 @@
-CREATE TABLE states (
-  id INT PRIMARY KEY,
-  name VARCHAR(256)
-);
+-- Creates the database and cities table with a foreign key to states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 
-CREATE TABLE cities (
-  id INT PRIMARY KEY,
-  state_id INT,
-  name VARCHAR(256),
-  FOREIGN KEY (state_id) REFERENCES states(id)
+CREATE TABLE IF NOT EXISTS cities (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
